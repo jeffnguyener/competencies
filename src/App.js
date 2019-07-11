5b1e7254cfa10505a8f04e93a2977cdab4f242ae
Jeff Nguyen <jeffnguyener@Samanthas-MacBook-Pro-2.local>
2019-07-10 23:05:33 -0600

    added new comp & router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react'
+
+import Nav from './Components/Nav'
+import { HashRouter } from 'react-router-dom'
+import router from './router'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <HashRouter>
+        <Nav />
+         {router}
+      </HashRouter>
+    )
+  }
 }
-
-export default App;
